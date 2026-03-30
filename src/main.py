3cf3c6b10bd002b142bc40dd30693f01bbdf35ea
Enzo Kozonoe <enzokozonoe@gmail.com>
2026-03-30 18:58:46 -0300

    Implementing detailed logs

--- src/main.py
+++ src/main.py
@@ -1,25 +1,32 @@
 from utils.reader import read_repository_file
 from core.analyzer import CodeAnalyzer
+from utils.logger import logger
 
 def main():
     path_to_analyze = "data/raw"
-    print(f"Lendo os arquivos do repositório: {path_to_analyze}")
+    logger.info(f"Lendo os arquivos do repositório: {path_to_analyze}")
     arquivos = read_repository_file(path_to_analyze)
 
     if not arquivos:
-        print(f"Nenhum arquivo encontrado para análise em {path_to_analyze}. Verifique o caminho e tente novamente.")
+        # Usa warning (aviso) para situações que não quebram o app, mas exigem atenção
+        logger.warning(f"Nenhum arquivo encontrado para análise em {path_to_analyze}. Verifique o caminho e tente novamente.")
         return
 
-    print(f"Analisando {len(arquivos)} arquivos com LLM...")
+    logger.info(f"Analisando {len(arquivos)} arquivos com LLM...")
     analyzer = CodeAnalyzer()
     relatorio = analyzer.generate_summary(arquivos)
 
     # Salva o resultado na pasta output
     output_path = "data/output/analise_tecnica.md"
-    with open(output_path, "w", encoding="utf-8") as f:
-        f.write(relatorio)
-    
-    print(f"Análise concluída! Veja o resultado em: {output_path}")
+
+    try: 
+        with open(output_path, "w", encoding="utf-8") as f:
+            f.write(relatorio)
+        
+        logger.info(f"Análise concluída! Veja o resultado em: {output_path}")
+    except Exception as e:
+        # Usa error para falhas que impedem o funcionamento correto do app
+        logger.error(f"Erro ao salvar o relatório: {str(e)}")
 
 if __name__ == "__main__":
     main()
